35b694250a3dce66ef6914deb550bc6067bb608c
mkaroi <88018119+mkaroi@users.noreply.github.com>
2022-05-19 17:44:55 +0700

    CMD 1.0RT

---
+++ %cmd%/scrip/a.py
@@ -0,0 +1,8 @@
+import subprocess
+
+def say(a):
+    print(a)
+
+def notepad():
+    subprocess.call("notepad")
+
---
+++ %cmd%/scrip/main.py
@@ -0,0 +1,136 @@
+
+from calendar import c
+import subprocess
+import sys
+import time
+import a
+current_time = time.localtime()
+import time
+import webbrowser as wb
+print("RUNTIME",current_time.tm_hour,":",current_time.tm_min,":",current_time.tm_sec)
+print("cmd2.6")
+print("Importing lib...")
+time.sleep(1)
+print("Loading script....")
+time.sleep(3)
+print("Finishing.....")
+time.sleep(2)
+n=input("name: ")
+i=input("%cmd%/bin/>> ")
+jj = 44
+
+while(jj == 44):
+
+    if i == "hello":
+        print("Hello", n)
+        i = None 
+        i = input("%cmd%/bin/>> ")
+    elif i == "none":
+        i = None 
+        i = input("%cmd%/bin/>> ")
+    elif i == "notepad":
+        i = None 
+        a.notepad()
+        i = input("%cmd%/bin/>> ")
+    elif i == "open-file":
+        i = None 
+        ij=input("Enter directory (with file name) : ")
+        subprocess.Popen(ij)
+        i=input("%cmd%/bin/>> ")
+    elif i == "PRANK2":
+        i = None 
+        wb.open('https://www.youtube.com/watch?v=x9aNNZY2lrY')
+        i = input("%cmd%/bin/>> ")
+    elif i == "secret":
+        i = None 
+        wb.open('https://www.youtube.com/channel/UCU5CZoYO0jNgrqTXYrhctrw')
+        i = input("%cmd%/bin/>> ")
+    elif i == "virusim":
+        i = None 
+        time.sleep(2)
+        print("---------------installer---------------------------")
+        print("install virus.pak 100%")
+        print("---------------------------------------------------")
+        time.sleep(1)
+        print("Detetor have detec file 'VIRC4' and found virus")
+        time.sleep(3)
+        print("can't delete virus")
+        time.sleep(3)
+        print()
+        print("------------Error-report-------------")
+        print("The program ran into the problem. ")
+        print("We will check a problem and end ")
+        print("program for you.")
+        print("STOPCODE 'VIRUS_DET_CAN_DELV_ERR551 '")
+        print("ERRORTIME",current_time.tm_hour,":",current_time.tm_min,":",current_time.tm_sec)
+        print("-------------------------------------")
+        print("END")
+        sys.exit()
+    elif i == "url":
+        i = None 
+        urltj = input("enter url: ")
+        wb.open(urltj)
+    elif i == "malwaresim":
+        i = None 
+        time.sleep(4)
+        print("---------------installer---------------------------")
+        print("install malvajava.pak 100%")
+        print("---------------------------------------------------")
+        time.sleep(1)
+        print("Detetor have detec file 'malvagoodserv450' and found  malware")
+        time.sleep(3)
+        print("can't delete malware")
+        time.sleep(3)
+        print()
+        print("------------Error-report-------------")
+        print("The program ran into the problem. ")
+        print("We will check a problem and end ")
+        print("program for you.")
+        print("STOPCODE 'VIRUS_DET_CAN_DELV_ERR551 '")
+        print("ERRORTIME",current_time.tm_hour,":",current_time.tm_min,":",current_time.tm_sec)
+        print("-------------------------------------")
+        print("END")
+        sys.exit()
+    elif i == "endses":
+        i = None 
+        print("ENDTIME",current_time.tm_hour,":",current_time.tm_min,":",current_time.tm_sec)
+        print("END")
+        sys.exit()
+    elif i == "open-link":
+        i = None 
+        uu=input("Enter link: ")
+        print("openning..")
+        time.sleep(2)
+        wb.open(uu)
+        i = input("%cmd%/bin/>> ")
+    elif i == "PRANK":
+        wb.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+        i = None 
+        i = input("%cmd%/bin/>> ")
+    elif i == "reload":
+        i = None 
+        n = None
+        print("restarting...")
+        time.sleep(5)
+        print("cmd2.6")
+        print("Importing venv...")
+        time.sleep(1)
+        print("Loading script....")
+        time.sleep(3)
+        print("Finishing.....")
+        time.sleep(4)
+        print("Complete please wait...")
+        time.sleep(2)
+        print("(if script error the program will end)")
+        n = input("name: ")
+        i = input("%cmd%/bin/>> ")
+    elif i == "info":
+        i = None 
+        print("Create 2-10-2021")
+        print("Made in thailand")
+        print("Made with python")
+        i = input("%cmd%/bin/>> ")
+    else:
+        i = None 
+        print("Invalid command")
+        i = input("%cmd%/bin/>> ")
